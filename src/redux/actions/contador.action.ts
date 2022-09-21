4a31005dc1830bf334f391e2c61376312077f309
GabrielHDZ <gabrielhdz0102@gmail.com>
2022-09-20 21:54:13 -0500

    update

--- src/redux/actions/contador.action.ts
+++ src/redux/actions/contador.action.ts
@@ -1,21 +1,28 @@
 
-interface actionUno{
+interface actionUno {
     type: string,
-    payload:string
+    payload: string
 }
 //is a object
-const addTodoAction:actionUno = {
+const addTodoAction: actionUno = {
     type: 'todos/todoAdded',
     payload: 'Buy milk'
 }
 //action creator is a function
-const addTodo= (text:string) => {
+const addTodo = (text: string) => {
     return {
-      type: 'todos/todoAdded',
-      payload: text
+        type: 'todos/todoAdded',
+        payload: text
     }
 }
-  
 
-  
-export {addTodoAction,addTodo}
+function suma(numero: number = 0): void {
+    var numero1 = 5;
+    var array = [4, 3, 2, 7];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+    }
+}
+
+export { addTodoAction, addTodo }
